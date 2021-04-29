e6d5ad1af2bf202d9c52080e9ed50a64d7d7df3e
AndreWeb <andzion105@gmail.com>
2021-04-29 21:33:08 +0300

    add nav

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,26 +9,33 @@ import './App.css';
 export const ClickedContext = React.createContext(false);
 
 export default class App extends Component {
-
   constructor(props) {
     super(props);
     this.myRef = React.createRef();
   }
 
   render() {
-    console.log(this.myRef)
+    console.log(this.myRef);
     return (
-      <Router>
-        <div className='App'>
-          <Header />
-          <h1>111</h1>
-          <Switch>
-            <Route exact path='/' render={() => <h1>Hello!</h1>} />
-            <Route path='/car' component={Car} />
-            <Route path='/counter' component={Counter} />
-          </Switch>
-        </div>
-      </Router>
+      // <Router>
+      //   <div className='App'>
+      //     <Header />
+      //     <h1>111</h1>
+      //     <Switch>
+      //       <Route exact path='/' render={() => <h1>Hello!</h1>} />
+      //       <Route path='/car' component={Car} />
+      //       <Route path='/counter' component={Counter} />
+      //     </Switch>
+      //   </div>
+      // </Router>
+
+      <ul>
+        <li>
+          <a href='/'>Главная</a>
+          <a href='Books'>Учебники</a>
+          <a href='Blog'>Блог</a>
+        </li>
+      </ul>
     );
   }
 }
